c6fd9e4a847a58e08e937150965874f036695fd5
imkewlhuh <rafael.lopez0827@gmail.com>
2023-03-24 22:37:53 -0400

    testimonialtesting

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -18,7 +18,7 @@ export const testimonials = [
 export default function Testimonial(props) {
     return (
         <>
-            <Avatar sx={{height: {xs: "5em", sm: "12em", md: "30vh"}, width: {xs: "5em", sm: "12em", md: "30vh"}}} alt={props.name} src={props.img} />
+            <Avatar sx={{height: {xs: "8em", sm: "12em", md: "30vh"}, width: {xs: "8em", sm: "12em", md: "30vh"}}} alt={props.name} src={props.img} />
             <Box sx={{width: {xs: "90%", md: "38%"}, fontSize: {sm: "18px"}}}>
                 <h1><span style={{ color: "#FFC20A" }}>WHAT OUR</span> STUDENTS HAVE TO SAY</h1>
                 <br />
